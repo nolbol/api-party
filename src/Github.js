e65121959543317a95d11ae8288f892281c1f9a8
nolan <nolan@nolans-mbp.coldspring.marian.edu>
2017-07-11 15:35:23 -0400

    make route for matching github username

---
+++ src/Github.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './Github.css'
+import { Route } from 'react-router-dom'
+
+class Github extends Component {
+    state = {
+        username: ''
+    }
+
+handleChange = (ev) => {
+    this.setState({ username: ev.target.value})
+}   
+
+handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+    this.setState({username: '' })
+}
+  render() {
+    return (
+      <div className="github">
+        <img 
+          className="github-logo" 
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="github logo"
+        />
+        <form onSubmit={this.handleSubmit}>
+
+          <div>
+            <input type="text" 
+            value={this.state.username} 
+            onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Look up Github user</button>
+          </div>
+        </form>
+        <Route path="/github/:username" render={(props) => <h3>You searched for {props.match.params.username}</h3> }/>
+        <Route exact path="/github" render={() => <h3>Please entter a username to search on Github.</h3>}/>
+        
+        </div>
+    )
+  }
+}
+
+export default Github
+
