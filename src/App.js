439b9be96779b44d69450ab2950d2bf770e080b4
nolan <nolan@nolans-mbp.coldspring.marian.edu>
2017-07-11 15:08:11 -0400

    added route render paths

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <div calssName="App-headings">
+          <div className="App-headings">
             <h3>Ain't no party like an</h3>
             <h1>API Party</h1>
-            </div>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to="/github">Github API</NavLink>
+            </li>
+          </ul>
         </div>
+        <Switch>
+          <Route path='/github' render={() => <h1>Github!</h1>} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
